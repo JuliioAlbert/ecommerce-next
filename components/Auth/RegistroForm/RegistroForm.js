e4c1798af7445b598f76240bac8cb97f606ec5c3
Julio Gonzalez <julioalbert98@gmail.com>
2021-01-28 13:39:32 -0600

    Validacion

--- components/Auth/RegistroForm/RegistroForm.js
+++ components/Auth/RegistroForm/RegistroForm.js
@@ -1,19 +1,28 @@
 import { useState } from 'react';
 import {Form,Button} from 'semantic-ui-react';
+import {useFormik } from 'formik';
+import * as Yup from 'yup';
+
 
 const RegistroForm = (props) => {
     const {showLoginForm} = props;
+
+    const formik = useFormik({
+            initialValues: initiaValues(),
+            validationSchema:Yup.object(validationSchema()),
+            onSubmit: (formData) => {
+                console.log(formData);
+            }
+        }
+    );
+
     return ( 
-        <Form className="login-form">
-            <Form.Input
-                name="name"
-                input="text"
-                placeholder="nombre"
-             />
-             <Form.Input name="apellido" type="text" placeholder="Apellidos" />
-             <Form.Input name="username" type="text" placeholder="Nombre de Usuario" />
-             <Form.Input name="email" type="text" placeholder="Correo Electronico" />
-             <Form.Input name="password" type="password" placeholder="Contraseña" />
+        <Form className="login-form" onSubmit={formik.handleSubmit}>
+             <Form.Input name="nombre" input="text"  placeholder="nombre"  onChange={formik.handleChange} error={formik.errors.nombre}/>
+             <Form.Input name="apellido" type="text" placeholder="Apellidos"  onChange={formik.handleChange} error={formik.errors.apellido}/>
+             <Form.Input name="userName" type="text" placeholder="Nombre de Usuario" onChange={formik.handleChange} error={formik.errors.userName} />
+             <Form.Input name="email" type="text" placeholder="Correo Electronico" onChange={formik.handleChange} error={formik.errors.email} />
+             <Form.Input name="password" type="password" placeholder="Contraseña" onChange={formik.handleChange} error={formik.errors.password}/>
              <div className="actions">
                  <Button type="button" basic >
                     Iniciar Sesion
@@ -33,3 +42,23 @@ const RegistroForm = (props) => {
 }
  
 export default RegistroForm;
+
+function initiaValues () {
+    return {
+        nombre:"",
+        apellido:"",
+        userName :"",
+        email: "",
+        password: ""
+    }
+}
+
+function validationSchema(){
+    return {
+        nombre: Yup.string().required(true),
+        apellido: Yup.string().required('El Apellido es Obligatorio'),
+        userName:Yup.string().required(true),
+        email: Yup.string().email().required(true),
+        password: Yup.string().required(true)
+    }
+}
