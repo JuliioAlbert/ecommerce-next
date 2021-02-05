7375145634ee83ea6b9f5324aac93e62d47e8b8a
Julio Gonzalez <julioalbert98@gmail.com>
2021-02-05 15:48:02 -0600

    Formik

--- components/Account/ChangeNameForm/ChangeNameForm.js
+++ components/Account/ChangeNameForm/ChangeNameForm.js
@@ -1,12 +1,41 @@
-import {Form, Button} from 'semantic-ui-react';
-const ChangeNameForm = ({user}) => {
-    return ( 
+import { Form, Button } from 'semantic-ui-react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import {toast} from 'react-toastify';
+
+
+const ChangeNameForm = (/*{user} */) => {
+    const user = {
+        nombre:'julio',
+        apellido:'Gonzalez'
+    }
+    const formik = useFormik({
+        initialValues: initialValues(user.nombre, user.apellido),
+        validationSchema: Yup.object(validationSchema()),
+        onSubmit: (values) => {
+            console.log(values)
+        }
+    });
+
+
+    return (
         <div className="change-name-form">
             <h4>Cambia Nombre y Apellidos</h4>
-            <Form>
+            <Form onSubmit={formik.handleSubmit}>
                 <Form.Group widths="equal">
-                    <Form.Input name="nombre" placeholder="Tu nombre"/>
-                    <Form.Input name="apellidos" placeholder="Tus Apellidos" />                    
+                    <Form.Input 
+                        onChange={formik.handleChange} 
+                        value={formik.values.nombre} 
+                        name="nombre" 
+                        error={formik.errors.nombre}
+                        placeholder="Tu nombre" />
+                        
+                    <Form.Input 
+                        onChange={formik.handleChange} 
+                        value={formik.values.apellido}
+                        name="apellido" 
+                        error={formik.errors.apellido}
+                        placeholder="Tus Apellidos" />
                 </Form.Group>
                 <Button className="submit">
                     Actualizar
@@ -14,7 +43,21 @@ const ChangeNameForm = ({user}) => {
             </Form>
 
         </div>
-     );
+    );
 }
- 
+
 export default ChangeNameForm;
+
+const initialValues = (nombre,apellido) => {
+    return {
+        nombre: nombre || '',
+        apellido: apellido || '',
+    }
+}
+
+const validationSchema= () =>{
+    return {
+        nombre: Yup.string().required(true),
+        apellido: Yup.string().required(true)
+    }
+}
